ab92bfd951f59f05d37ce6114d1a76a7ca2cd026
João Feck <jl.feck@gmail.com>
2024-05-19 21:42:19 -0300

    test maven

--- .pipelines/release.groovy
+++ .pipelines/release.groovy
@@ -52,7 +52,7 @@ node(jenkinsNode) {
 
       // Creates the build artifact.
       stage('maven-build') {
-        sh("./mvnw package -Pnative -s settings.xml")
+        sh("mvn package -Pnative -s settings.xml")
       }
 
       docker.withRegistry("https://${env.DOCKER_REGISTRY_URL}", env.DOCKER_REGISTRY_CREDENTIALS_ID) {
